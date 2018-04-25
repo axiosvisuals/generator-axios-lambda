8f6016c0885c68a4f7de734487752b5f7d2b231b
geraldarthur <gerald.a.rich@gmail.com>
2018-04-25 10:37:09 -0400

    simplify s3 upload

--- generators/go/templates/functions/lambda_tmpl/utils/upload_to_s3.go
+++ generators/go/templates/functions/lambda_tmpl/utils/upload_to_s3.go
@@ -1,26 +1,20 @@
 package utils
 
 import (
-	"context"
 	"fmt"
 	"io"
-	"time"
 
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/awserr"
-	"github.com/aws/aws-sdk-go/aws/request"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
+	"github.com/aws/aws-sdk-go/service/s3/s3manager"
 )
 
-// Uploads a file to S3 given a reader, bucket and object key. Also takes a duration
-// value to terminate the update if it doesn't complete within that time.
-//
+// Uploads a file to S3 given a reader, bucket and object filename.
 // The AWS Region needs to be provided in the AWS shared config or on the
 // environment variable as `AWS_REGION`. Credentials also must be provided
 // Will default to shared config file, but can load from environment if provided.
 //
-func main(r io.ReadSeeker, bucket string, key string, timeout time.Duration) {
+func main(r io.ReadSeeker, bucket string, filename string) {
 	// All clients require a Session. The Session provides the client with
 	// shared configuration such as region, endpoint, and credentials. A
 	// Session should be shared where possible to take advantage of
@@ -32,35 +26,16 @@ func main(r io.ReadSeeker, bucket string, key string, timeout time.Duration) {
 	// Optional aws.Config values can also be provided as variadic arguments
 	// to the New function. This option allows you to provide service
 	// specific configuration.
-	svc := s3.New(sess)
+	uploader := s3manager.NewUploader(sess)
 
-	// Create a context with a timeout that will abort the upload if it takes
-	// more than the passed in timeout.
-	ctx := context.Background()
-	var cancelFn func()
-	if timeout > 0 {
-		ctx, cancelFn = context.WithTimeout(ctx, timeout)
-	}
-	// Ensure the context is canceled to prevent leaking.
-	// See context package for more information, https://golang.org/pkg/context/
-	defer cancelFn()
-
-	// Uploads the object to S3. The Context will interrupt the request if the
-	// timeout expires.
-	_, err := svc.PutObjectWithContext(ctx, &s3.PutObjectInput{
+	_, err := uploader.Upload(&s3manager.UploadInput{
 		Bucket: aws.String(bucket),
-		Key:    aws.String(key),
+		Key:    aws.String(filename),
 		Body:   r,
 	})
 	if err != nil {
-		if aerr, ok := err.(awserr.Error); ok && aerr.Code() == request.CanceledErrorCode {
-			// If the SDK can determine the request or retry delay was canceled
-			// by a context the CanceledErrorCode error code will be returned.
-			fmt.Printf("upload canceled due to timeout, %v\n", err)
-		} else {
-			fmt.Printf("failed to upload object, %v\n", err)
-		}
-	}
 
-	fmt.Printf("successfully uploaded file to %s/%s\n", bucket, key)
+		fmt.Printf("failed to upload object, %v\n", err)
+	}
+	fmt.Printf("successfully uploaded file to %s/%s\n", bucket, filename)
 }
